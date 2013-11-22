d0842cd56323a1101cacc6af8de01cac88966a59
birkin <birkin.diana@gmail.com>
2013-11-22 17:13:15 -0500

    queried fedora and solr.

--- acc_num_to_pid.py
+++ acc_num_to_pid.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import json
+import json, pprint
+import requests
 
 
 class PidFinder( object ):
@@ -9,21 +10,18 @@ class PidFinder( object ):
         bdr_collection_pid,
         bell_dict_json_path,
         fedora_risearch_url,
-        output_json_path,
-        studio_solr_root_url ):
-        """ Controller.
+        output_json_path ):
+        """ CONTROLLER.
             Returns accession-number dict showing bdr-pid & title.
-            Example: { acc_num_1: {pid:123, state:active, title:cool_pic}, acc... etc. }
+            Example: { acc_num_1: {bdr:123, state:active}, acc_num_2: {bdr:234, state:active}, etc. }
             Gets collection members from fedora, checks studio-solr for info. """
         #Run itql query
-        itql_query_output = self.run_itql_query( fedora_risearch_url, bdr_collection_pid )
+        itql_query_output = self._run_itql_query( fedora_risearch_url, bdr_collection_pid )
         #Parse results to fedora-pid list
-        fedora_pid_list = self.parse_itql_search_results( itql_query_output )
-        #Run studio-solr query
-        studio_solr_output = self.run_studio_solr_query( studio_solr_root_url, bdr_collection_pid )
-        #Parse results to solr-pid: title
-        studio_pid_dict = self.parse_studio_solr_results( studio_solr_output )
-        #Make fedora pid-dict...
+        fedora_pid_list = self._parse_itql_search_results( itql_query_output )
+        #Run studio-solr query for solr-pid list
+        studio_solr_pid_list = self._run_studio_solr_query( bdr_collection_pid )
+        #Make fedora pid-dict... ( intersect for active/inactive ) { bdr123: active, bdr234: active }
         fedora_pid_dict = self.make_fedora_pid_dict( fedora_pid_list, studio_pid_dict )
         #Get accession numbers
         accession_numbers = sorted(json.loads(bell_dict_json_path).keys() )
@@ -32,84 +30,41 @@ class PidFinder( object ):
         #Output json
         pass
 
+    def _run_itql_query( self, fedora_risearch_url, bdr_collection_pid ):
+        """ Returns output from fedora itql resource-index search. """
+        itql_query = u'select $object from <#ri> where $object <fedora-rels-ext:isMemberOf> <info:fedora/%s>' % bdr_collection_pid
+        params = { u'lang': u'itql', u'format': u'json', u'query': itql_query }
+        try:
+          r = requests.get( fedora_risearch_url, params=params, verify=False )
+          d = json.loads( r.text )
+          assert d.keys() == [ u'results' ]
+          results_list = d[u'results']
+          return results_list
+        except Exception, e:
+          message = u'problem running itql search: %s' % repr(e).decode(u'utf-8', u'replace')
+          raise Exception( message )
 
-    def make_fedora_pid_dict( self, fedora_pid_list, studio_pid_dict ):
-        """ Returns pid-dict
-            Example: { pid1: {'state': 'active', 'title': 'abc'}, pid2: {etc} } """
-        fedora_pid_dict = {}
-        for fedora_pid in fedora_pid_list:
-            #Set 'state' and 'title'
-            if fedora_pid in studio_pid_dict.keys():
-                final_dict[fedora_pid] = { u'state': u'active', u'title': studio_pid_dict[fedora_pid] }
-            else:
-                final_dict[fedora_pid] = { u'state': u'inactive', u'title': None }
+    def _parse_itql_search_results(self, itql_query_output ):
+        """ Returns list of pids from _fedora_. """
+        assert type( itql_query_output ) == list
+        parsed_pids = []
+        for entry in itql_query_output:  # eg entry: { u'object': u'info:fedora/bdr:1234' }
+          if entry[u'object'][0:4] == u'info':
+            parsed_pids.append( entry[u'object'].split(u'/')[1] )
+        sorted_fedora_pids = sorted( parsed_pids )
+        return sorted_fedora_pids
 
-
-
-        ## setup
-        bh = BellHelper()
-        output = {
-          u'count': 0,
-          u'accession_numbers': {}  # u'accession_number': { u'title': u'the_title', u'pid': u'the_pid' }
-          }
-        ## get collection children list
-        unparsed_child_pids = bh.run_collection_member_search( self.FEDORA_RISEARCH_URL , self.COLLECTION_PID )
-        ## parse list
-        cleaned_child_pids = bh.parse_itql_search_results( unparsed_child_pids )
-        # print u'- cleaned_child_pids...'; pprint.pprint( cleaned_child_pids )
-        ## save
-        t = Task.objects.get( name=u'list_previously_ingested_pids', project=u'BELL201305' )
-        t.output = json.dumps( sorted(cleaned_child_pids), indent=2, sort_keys=True )
-        t.save()
-        return
-        # end def list_previously_ingested_pids()
-
-
-      # def run_collection_member_search( self, risearch_url, collection_pid ):
-      #   """ Runs an itql query to get a list of member objects.
-      #       Called by bell_2013_05.build_ingested_list() """
-      #   itql_query = u'select $object from <#ri> where $object <fedora-rels-ext:isMemberOf> <info:fedora/%s>' % collection_pid
-      #   params = {
-      #     u'lang': u'itql',
-      #     u'format': u'json',
-      #     u'query': itql_query,
-      #     }
-      #   try:
-      #     r = requests.get( risearch_url, params=params, verify=False )
-      #     d = json.loads( r.text )
-      #     assert d.keys() == [ u'results' ]
-      #     results_list = d[u'results']
-      #     return results_list
-      #   except Exception, e:
-      #     message = u'problem running itql search: %s' % repr(e).decode(u'utf-8', u'replace')
-      #     log.error( message )
-      #     raise Exception( message )
-
-
-
-
-# def list_pids( self ):
-# """ Returns accession-number dict showing bdr-pid & title.
-#     Gets collection members from fedora, checks studio-solr for info. """
-#     assert type(self.FEDORA_RISEARCH_URL) == unicode and len(self.FEDORA_RISEARCH_URL) > 0
-#     assert type(self.COLLECTION_PID) == unicode and len(self.COLLECTION_PID) > 0
-#     ## setup
-#     bh = BellHelper()
-#     output = {
-#       u'count': 0,
-#       u'accession_numbers': {}  # u'accession_number': { u'title': u'the_title', u'pid': u'the_pid' }
-#       }
-#     ## get collection children list
-#     unparsed_child_pids = bh.run_collection_member_search( self.FEDORA_RISEARCH_URL , self.COLLECTION_PID )
-#     ## parse list
-#     cleaned_child_pids = bh.parse_itql_search_results( unparsed_child_pids )
-#     # print u'- cleaned_child_pids...'; pprint.pprint( cleaned_child_pids )
-#     ## save
-#     t = Task.objects.get( name=u'list_previously_ingested_pids', project=u'BELL201305' )
-#     t.output = json.dumps( sorted(cleaned_child_pids), indent=2, sort_keys=True )
-#     t.save()
-#     return
-#     # end def list_previously_ingested_pids()
+    def _run_studio_solr_query( self, bdr_collection_pid ):
+        """ Returns list of pids from _solr_. """
+        collection_api_url = u'https://repository.library.brown.edu/api/pub/collections/%s/' % bdr_collection_pid
+        r = requests.get( collection_api_url )
+        d = json.loads( r.text )
+        assert( sorted(d.keys()) ) == [ u'count', u'id', u'items' ]
+        pid_list = []
+        for entry in d[u'items'][u'docs']:  # entry: { u'pid': u'bdr:1234' }
+            pid_list.append( entry[u'pid'] )
+        sorted_solr_pids = sorted( pid_list )
+        return sorted_solr_pids
 
 
 
@@ -121,6 +76,5 @@ if __name__ == u'__main__':
     bdr_collection_pid=bs.COLLECTION_PID,
     bell_dict_json_path=bs.BELL_DICT_JSON_PATH,
     fedora_risearch_url=bs.FEDORA_RISEARCH_URL,
-    output_json_path=bs.OUTPUT_JSON_PATH,
-    studio_solr_root_url=bs.STUDIO_SOLR_ROOT_URL
+    output_json_path=bs.OUTPUT_JSON_PATH
     )
