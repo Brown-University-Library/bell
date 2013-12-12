2d97cd597780f27fd153598b8f3841801506d164
birkin <birkin.diana@gmail.com>
2013-12-12 15:59:28 -0500

    added logger

---
+++ bell_logger.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import logging, os
+
+def setup_logger():
+    """ Configures a logger to write to console & <filename>. """
+    formatter = logging.Formatter(u'%(asctime)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(u'bell_logger')
+    logger.setLevel(logging.DEBUG)
+    filename = u'%s/bell.log' % os.environ.get(u'BELL_LOG_DIR')
+    file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=(5*1024*1024), backupCount=1)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    # console_handler = logging.StreamHandler()
+    # console_handler.setLevel(logging.DEBUG)
+    # console_formatter = logging.Formatter(u'%(levelname)s - %(message)s')
+    # console_handler.setFormatter(console_formatter)
+    # logger.addHandler(console_handler)
+    return logger
--- tasks/check_environment.py
+++ tasks/check_environment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import datetime, json, os, pprint, sys
+import bell_logger
 import redis
 
 from redis import Redis
@@ -14,13 +15,17 @@ q = Queue( u'bell_work', connection=Redis() )
 
 def check_redis_status_dict():
     """ Checks that redis is running. """
+    logger = bell_logger.setup_logger()
+    logger.info( u'STARTING_PROCESSING...' )
     try:
         assert len(r.keys()) > -1  # if redis isn't running this will generate an error
         next = next_task.determine_next_task( sys._getframe().f_code.co_name )  # passes current function name
         job = q.enqueue_call ( func=u'%s' % next, args = (), timeout = 30 )
         return
     except Exception as e:
-        raise Exception( u'Redis does not appear to be running; exception: %s' % unicode(repr(e)) )
+        message = u'Redis does not appear to be running; exception: %s' % unicode(repr(e))
+        logger.error( message )
+        raise Exception( message )
 
 
 def archive_previous_work():
@@ -43,11 +48,14 @@ def archive_previous_work():
 def check_main_accession_number_dict():
     """ Checks that 'accession_number_to_data_dict.json' exists.
         Possible TODO: load it to ensure it's a dict. """
-    core_accession_number_file_path = u'init'
-    try:
-        core_accession_number_file_path = unicode( os.environ.get(u'BELL_CE__BELL_DICT_JSON_PATH') )
-        assert os.path.exists( core_accession_number_file_path )
-    except Exception as e:
-        message = u'Problem finding "accession_number_to_data_dict.json" at %s; exception: %s; environ: %s' % (
-            core_accession_number_file_path, unicode(repr(e)), os.environ )
-        raise Exception( message )
+    logger = bell_logger.setup_logger()
+    for filepath in [ os.environ.get(u'BELL_CE__BELL_DICT_JSON_PATH'), os.environ.get(u'BELL_CE__AccToPidDict_JSON_PATH') ]:
+        try:
+            assert os.path.exists( filepath )
+            logger.info( u'- filepath %s ok' % filepath )
+            logger.info( u'abc' )
+        except Exception as e:
+            message = u'Problem finding filepath %s; exception: %s' % ( filepath, unicode(repr(e)) )
+            logger.error( message )
+            raise Exception( message )
+    return
