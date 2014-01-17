d98c51aee65aa35f069e76da6c4144b501529d59
birkin <birkin.diana@gmail.com>
2014-01-17 14:24:54 -0500

    debugged.

--- utils/delete_dev_collection_pids.py
+++ utils/delete_dev_collection_pids.py
@@ -19,29 +19,29 @@ from rq import Queue
 
 def get_collection_pids():
     """ Gets list of pids to delete and puts each deletion-job on queue. """
-    ( pid_finder, COLLECTION_PID, MEMBERSHIP_URL, DELETION_URL, queue_name,logger ) = _setup_getter_vars()
+    ( pid_finder, COLLECTION_PID, MEMBERSHIP_URL, QUEUE_NAME, logger ) = _setup_getter_vars()
     logger.info( u'in delete_dev_collection_pids.get_collection_pids(); starting' )
-    fedora_pid_list = _run_fedora_itql_query( queue_name, DELETION_URL )
-    _populate_queue( fedora_pid_list )
+    fedora_pid_list = _run_fedora_itql_query( pid_finder, MEMBERSHIP_URL, COLLECTION_PID, logger )
+    _populate_queue( fedora_pid_list, QUEUE_NAME, logger )
     logger.info( u'in delete_dev_collection_pids.get_collection_pids(); done' )
     return
 
 
 def _setup_getter_vars():
     """ Returns logger, and required parameters after setting and checking them. """
-    ( COLLECTION_PID, MEMBERSHIP_URL, DELETION_URL, queue_name, logger ) = (
+    ( COLLECTION_PID, MEMBERSHIP_URL, QUEUE_NAME ) = (
         unicode( os.environ.get(u'BELL_UTILS__COLLECTION_PID') ),
         unicode( os.environ.get(u'BELL_UTILS__FEDORA_RISEARCH_URL') ),
-        unicode( os.environ.get(u'BELL_UTILS__FEDORA_ADMIN_URL') ),
-        bell_logger.setup_logger() )
+        unicode( os.environ.get(u'BELL_QUEUE_NAME') )
+        )
     assert u'test' in COLLECTION_PID, Exception( u'in delete_dev_collection_pids(); ERROR: problem validating collection-pid' )
     assert u'dev' in MEMBERSHIP_URL, Exception( u'in delete_dev_collection_pids(); ERROR: problem validating membership-url' )
-    assert u'dev' in DELETION_URL, Exception( u'in delete_dev_collection_pids(); ERROR: problem validating deletion-url' )
     pid_finder = PidFinder()
-    return ( pid_finder, COLLECTION_PID, MEMBERSHIP_URL, DELETION_URL, queue_name, logger )
+    logger = bell_logger.setup_logger()
+    return ( pid_finder, COLLECTION_PID, MEMBERSHIP_URL, QUEUE_NAME, logger )
 
 
-def _run_fedora_itql_query( pid_finder, MEMBERSHIP_URL, COLLECTION_PID ):
+def _run_fedora_itql_query( pid_finder, MEMBERSHIP_URL, COLLECTION_PID, logger ):
     """ Returns list of pids from a fedora itql search. """
     itql_query_output = pid_finder._run_itql_query( fedora_risearch_url=MEMBERSHIP_URL, bdr_collection_pid=COLLECTION_PID )
     assert type( itql_query_output ) == list
@@ -52,9 +52,9 @@ def _run_fedora_itql_query( pid_finder, MEMBERSHIP_URL, COLLECTION_PID ):
     return fedora_pid_list
 
 
-def _populate_queue( fedora_pid_list ):
+def _populate_queue( fedora_pid_list, QUEUE_NAME, logger ):
     """ Creates a queue deletion-job for each pid. """
-    q = Queue( queue_name, connection=Redis() )
+    q = Queue( QUEUE_NAME, connection=Redis() )
     for i, pid in enumerate( fedora_pid_list ):
         data = { u'pid': pid }
         q.enqueue_call ( func=u'utils.delete_dev_collection_pids.task__delete_item_from_fedora', args =(data,), timeout=30 )
@@ -84,16 +84,16 @@ def task__delete_item_from_fedora( data ):
 
 def _setup_delete_vars( data ):
     """ Returns logger, and required parameters after setting and checking them. """
-    ( pid, fedora_url, fedora_username, fedora_password, logger ) = (
+    ( pid, fedora_url, fedora_username, fedora_password ) = (
         data[u'pid'],
         unicode( os.environ.get(u'BELL_UTILS__FEDORA_ADMIN_URL') ),
         unicode( os.environ.get(u'BELL_UTILS__FEDORA_ADMIN_USERNAME') ),
-        unicode( os.environ.get(u'BELL_UTILS__FEDORA_ADMIN_PASSWORD') ),
-        bell_logger.setup_logger() )
+        unicode( os.environ.get(u'BELL_UTILS__FEDORA_ADMIN_PASSWORD') ) )
     check_list = [ pid, fedora_url, fedora_username, fedora_password ]
     for var in check_list:
         position = check_list.index(var)
         assert type( var ) == unicode, Exception( u'var at position `%s` is `%s`, not unicode' % (position, var) )
+    logger = bell_logger.setup_logger()
     return ( pid, fedora_url, fedora_username, fedora_password, logger )
 
 
