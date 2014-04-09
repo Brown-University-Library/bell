f42bc2b050b5e0c7aa45bf91fa5063ef5efa0db5
birkin <birkin.diana@gmail.com>
2014-04-09 10:57:58 -0400

    adds interactivity.

--- utils/requeue_failed_bell_jobs.py
+++ utils/requeue_failed_bell_jobs.py
@@ -11,10 +11,19 @@ BELL_QUEUE = os.environ.get( u'BELL_QUEUE_NAME' )
 
 for job in failed_queue.jobs:
     if not job.origin == BELL_QUEUE:
-        print( u'skipping function call: %s' % job.func_name )
+        print( u'skipping non-bell function call: %s' % job.func_name )
         continue
     else:
-        print( u'requeueing function call: %s' % job.func_name )
-        # failed_queue.requeue( job.id )
+        print( u'function call: %s' % job.func_name )
+        action_val = raw_input( u'Action (use first letter from the following, default is nothing): [Nothing/Requeue/Delete] ' )
+        if action_val.lower() == u'r':
+            print('requeuing job...')
+            # failed_queue.requeue( job.id )
+        elif action_val.lower() == 'd':
+            print('deleting job...')
+            # failed_queue.remove( job.id )
+        else:
+            print('skipping job...')
+            pass
 
 sys.exit()
