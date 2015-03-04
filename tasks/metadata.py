bdfdf4501edd950506645db953272f1ee2a0fb3e
birkin <birkin.diana@gmail.com>
2015-03-04 15:36:41 -0500

    refactors metadata-only list into tasks module.

--- tasks/metadata.py
+++ tasks/metadata.py
@@ -2,7 +2,7 @@
 
 """ Handles metadata-related tasks. """
 
-import json, os, pprint, sys
+import datetime, json, os, pprint, sys
 import redis, rq
 from bell_code import bell_logger
 
@@ -11,30 +11,49 @@ q = rq.Queue( queue_name, connection=redis.Redis() )
 r = redis.StrictRedis( host='localhost', port=6379, db=0 )
 
 
-class MetadataHandler( object ):
-    """ Handles metadata-related tasks.
-        TODO: call determine_next_task() from runners, not from within class. """
+class MetadataOnlyLister( object ):
+    """ Creates json file of accession numbers for which new metatdata-only objects will be created. """
 
-    def __init__( self, logger ):
-        self.logger = logger
+    def __init__( self ):
+        self.PID_JSON_PATH = unicode( os.environ[u'BELL_UTILS__PID_DICT_JSON_PATH'] )
+        self.OUTPUT_PATH = unicode( os.environ[u'BELL_UTILS__METADATA_ONLY_ACCNUMS_JSON_PATH'] )
 
-    def create_metadata_only_listing( self ):
-        """ Loads necessary json files, prepares & returns acession-number list of metadata objects to be created.
-            Called manually. """
-        self.logger.debug( u'in metadata.MetadataHandler.create_metadata_only_listing(); starting' )
-        with open( unicode(os.environ.get(u'BELL_METADATA__PID_DICT_JSON_PATH')) ) as f:
+    def list_metadata_only_accession_numbers( self ):
+        """ Saves a json list of accession_numbers.
+            Called manuallly per readme. """
+        logger.debug( u'in utils.make_metadata_only_list.MetadataOnlyLister.list_metadata_only_accession_numbers(); starting' )
+        with open( self.PID_JSON_PATH ) as f:
             dct = json.loads( f.read() )
         dct_lst = sorted( dct[u'final_accession_pid_dict'].items() )
         lst_to_queue = []
         for (accession_number, pid) in dct_lst:
             if pid == None and not accession_number == "null":
                 lst_to_queue.append( accession_number.strip() )
-        return lst_to_queue
+        data = {
+            u'datetime': unicode( datetime.datetime.now() ), u'count': len( lst_to_queue ), u'accession_numbers': lst_to_queue }
+        self.output_listing( data )
+
+    def output_listing( self, data ):
+        """ Saves json file.
+            Called by list_metadata_only_accession_numbers() """
+        jsn = json.dumps( data, indent=2, sort_keys=True )
+        with open( self.OUTPUT_PATH, u'w' ) as f:
+            f.write( jsn )
+        return
+
+    # end class MetadataOnlyLister()
+
+
+class MetadataCreator( object ):
+    """ Handles metadata-related tasks. """
+
+    def __init__( self, logger ):
+        self.logger = logger
 
     def create_metadata_only_object( self, accession_number ):
         """ Gathers source metadata, prepares call to item-api, calls it, and confirms creation.
             Called by run_create_metadata_only_object() """
-        self.logger.debug( u'in metadata.MetadataHandler.create_metadata_only_object(); starting' )
+        self.logger.debug( u'in metadata.MetadataCreator.create_metadata_only_object(); starting' )
         # item_dct = self.grab_item_dct( accession_number )
         # rights_params = self.make_rights_params( item_dct )
         # ir_params = self.make_ir_params( item_dct )
@@ -47,37 +66,16 @@ class MetadataHandler( object ):
 
     ## helpers ##
 
-    # end class MetadataHandler()
+    # end class MetadataCreator()
 
 
 ## runners ##
 
 logger = bell_logger.setup_logger()
 
-def run_create_metadata_only_listing():
-    """ Runner for create_metadata_only_listing()
-        Called manually:
-        >>> from tasks import metadata
-        >>> metadata.run_create_metadata_only_listing() """
-    mh = MetadataHandler( logger )
-    metadata_only_lst = mh.create_metadata_only_listing()
-    pprint.pprint( metadata_only_lst )
-    return
-
-def run_create_metadata_only_listing_and_enqueue_jobs():
-    """ Runner for create_metadata_only_listing()
-        Called manually:
-        >>> from tasks import metadata
-        >>> metadata.run_create_metadata_only_listing() """
-    mh = MetadataHandler( logger )
-    metadata_only_lst = mh.run_create_metadata_only_listing_and_enqueue_jobs()
-    for accession_number in metadata_only_lst:
-        q.enqueue_call( func=u'bell_code.tasks.metadata.run_create_metadata_only_object', kwargs={u'accession_number': accession_number}, timeout=600 )
-    return
-
 def run_create_metadata_only_object( accession_number ):
     """ Runner for create_metadata_only_object()
-        Called by queued job triggered by MetadataHandler.create_metadata_only_listing() """
-    mh = mh = MetadataHandler( logger )
-    mh.create_metadata_only_object( accession_number )
+        Called by queued job triggered by... """
+    m = MetadataCreator( logger )
+    m.create_metadata_only_object( accession_number )
     return
