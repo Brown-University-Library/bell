06049697fcb82538b3fef5b3350f19bf3a77db56
birkin <birkin.diana@gmail.com>
2015-03-11 10:13:00 -0400

    preps queue-calling code.

--- utils/queue_utils/worker_start.sh
+++ utils/queue_utils/worker_start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Note:
+# When an app setting is changed for code called by a worker, restart the worker from the `bell_code` dir.
+#
+# Example of killing and restarting worker...
+#
+# $ ps aux | grep "rq"
+# ( examine output to find out worker PID for bell project)
+#
+# $ cd /path/to/bell_code/
+# $ source ../env/bin/activate
+# $ kill PID
+# $ bash ./utils/queue_utils/worker_start.sh
+#
+# $ ps aux | grep "rq"
+# (to confirm new worker is running)
+
 
 WORKER_NAME=bell_worker_$RANDOM
 LOG_FILENAME=$BELL_LOG_DIR/$WORKER_NAME.log
@@ -8,5 +24,5 @@ echo "worker name: " $WORKER_NAME
 echo "log filename: " $LOG_FILENAME
 echo "queue name: " $BELL_QUEUE_NAME
 
-# rqworker --name $WORKER_NAME $QUEUE_NAME >> $LOG_FILENAME 2>&1 &
-rqworker --name $WORKER_NAME $BELL_QUEUE_NAME
+rqworker --name $WORKER_NAME $QUEUE_NAME >> $LOG_FILENAME 2>&1 &
+# rqworker --name $WORKER_NAME $BELL_QUEUE_NAME
