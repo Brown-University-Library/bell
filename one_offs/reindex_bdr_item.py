c8003aa464b0de41889c33f9abc899a81c84d600
birkin <birkin.diana@gmail.com>
2015-04-02 15:52:13 -0400

    updates reindex-single-item.

--- one_offs/reindex_bdr_item.py
+++ one_offs/reindex_bdr_item.py
@@ -24,22 +24,30 @@ class BdrItemReindexer( object ):
     def reindex_bdr_item( self ):
         """ Manages source data-grab and reindexer call.
             Called manually. """
-        bell_dct = self.grab_bell_data()
+        api_dct = self.grab_api_dct()
+        bell_dct = self.grab_bell_dct()
         idxr.update_custom_index_entry( self.ACCESSION_NUMBER, bell_dct, self.PID )
         return
 
-    def grab_bell_data( self ):
-        """ Grabs bell json; returns dict.
+    def grab_api_dct( self ):
+        """ Grabs api data.
             Called by reindex_bdr_item() """
         url = u'%s/%s/' % ( self.BDR_PUBLIC_ITEM_API_URL_ROOT, self.PID )
-        logger.debug( u'in _grab_bell_data(); url, `%s`' % url )
+        logger.debug( u'in grab_api_dct(); url, `%s`' % url )
         r = requests.get( url )
         api_dct = r.json()
+        return api_dct
+
+    def grab_bell_dct( self, api_dct ):
+        """ Grabs source json & cleans accession_number.
+            Called by reindex_bdr_item() """
         bell_jsn_url = api_dct[u'links'][u'content_datastreams'][u'bell_metadata']
-        logger.debug( u'in _grab_bell_data(); bell_jsn_url, `%s`' % bell_jsn_url )
-        r2 = requests.get( bell_jsn_url )
+        logger.debug( u'in grab_bell_dct(); bell_jsn_url, `%s`' % bell_jsn_url )
+        r = requests.get( bell_jsn_url )
         bell_dct = r.json()
-        logger.debug( u'in _grab_bell_data(); bell_dct, `%s`' % pprint.pformat(bell_dct) )
+        src_accession_number = bell_dct[u'calc_accession_id']
+        bell_dct[u'calc_accession_id'] = src_accession_number.strip()
+        logger.debug( u'in grab_bell_dct(); bell_dct, `%s`' % pprint.pformat(bell_dct) )
         return bell_dct
 
     # end class BdrItemReindexer
