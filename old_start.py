f6fe31d8b14e43c3e7e5eda382b580c91cfed939
birkin <birkin.diana@gmail.com>
2016-03-28 15:37:56 -0400

    updates remaining files to use unicode-literals.

--- old_start.py
+++ old_start.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 """
 - Old code that assumed I'd run this start command and everything would automagically happen.
 - Revising approach March 2015. Since I run this so infrequently, I'm changing to the approach
@@ -15,10 +17,10 @@
 #     """
 
 
-# queue_name = os.environ.get(u'BELL_QUEUE_NAME')
+# queue_name = os.environ.get('BELL_QUEUE_NAME')
 # q = Queue( queue_name, connection=Redis() )
 # job = q.enqueue_call (
-#   func=u'bell_code.tasks.check_environment.ensure_redis',
+#   func='bell_code.tasks.check_environment.ensure_redis',
 #   args=(),
 #   timeout=30
 #   )
