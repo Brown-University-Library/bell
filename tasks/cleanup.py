2e1eaec1f1de07fdc19a97a844984c0deab0c08f
birkin <birkin.diana@gmail.com>
2016-11-03 14:13:39 -0400

    adds runner.

--- tasks/cleanup.py
+++ tasks/cleanup.py
@@ -167,5 +167,9 @@ def run_make_bdr_pids_to_delete():
     deleter = BdrDeleter()
     deleter.make_pids_to_delete()
 
+def run_delete_pid_from_bdr( pid ):
+    deleter = BdrDeleter()
+    deleter.delete_pid_via_bdr_item_api( pid )
+
 
 ## EOF
