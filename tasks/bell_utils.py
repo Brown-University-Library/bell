2d4b3ddf682eaace8c8d3d3cecb5e74d337c818d
Ben Cail <benjamin_cail@brown.edu>
2018-05-10 13:57:52 -0400

    Add bell_utils.py module. Add format_xml_file.py to tasks for formatting raw XML file.

---
+++ tasks/bell_utils.py
@@ -0,0 +1,7 @@
+import os
+
+
+TASKS_DIR = os.path.dirname(os.path.abspath(__file__))
+BELL_CODE_DIR = os.path.dirname(TASKS_DIR)
+DATA_DIR = os.path.join(BELL_CODE_DIR, 'data')
+
---
+++ tasks/format_xml_file.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from lxml import etree
+import bell_utils
+
+
+RAW_DATA_PATH = os.path.join(bell_utils.DATA_DIR, 'a__all_data_raw.xml')
+FORMATTED_DATA_PATH = os.path.join(bell_utils.DATA_DIR, 'b__all_data_formatted.xml')
+
+
+def create_formatted_xml_file():
+    with open(RAW_DATA_PATH, 'rb') as f:
+        data = f.read()
+    xml_doc = etree.fromstring(data)
+    formatted_data = etree.tostring(xml_doc, pretty_print=True)
+    with open(FORMATTED_DATA_PATH, 'wb') as f:
+        f.write(formatted_data)
+
+
+if __name__ == '__main__':
+    create_formatted_xml_file()
+
